6172e13079872a897e2a97d8bd8c349e5f65f1b8
KK-tech-byte1991 <84914086+KK-tech-byte1991@users.noreply.github.com>
2024-06-20 22:23:13 +0530

    added homepage

--- src/router.tsx
+++ src/router.tsx
@@ -7,6 +7,10 @@ import Register from "./pages/LoginRegister/Register";
 import Dashboard from "./pages/dashboard/dashboard";
 export const router = createBrowserRouter([
   {
+    path: "/",
+    element: <h1 >Welcome</h1>,
+  },
+  {
     path: "/login",
     element: <Login />,
   },
